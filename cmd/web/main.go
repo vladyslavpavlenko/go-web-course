5b8616efa1d3b22d49863440eb3fbf7f55a8b4c6
Vladyslav Pavlenko <vladyslavpavlen@gmail.com>
2023-09-21 17:36:11 +0300

    Set up sessions

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,17 +2,31 @@ package main
 
 import (
 	"fmt"
+	"github.com/alexedwards/scs/v2"
 	"github.com/vladyslavpavlenko/go-web-course/pkg/config"
 	"github.com/vladyslavpavlenko/go-web-course/pkg/handlers"
 	"github.com/vladyslavpavlenko/go-web-course/pkg/render"
 	"log"
 	"net/http"
+	"time"
 )
 
 const portNumber = ":8080"
 
+var app config.AppConfig
+var session *scs.SessionManager
+
 func main() {
-	var app config.AppConfig
+	// change this to true when in production
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
 
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
--- go.mod
+++ go.mod
@@ -2,6 +2,9 @@ module github.com/vladyslavpavlenko/go-web-course
 
 go 1.19
 
-require github.com/go-chi/chi/v5 v5.0.10
+require (
+	github.com/go-chi/chi/v5 v5.0.10
+	github.com/justinas/nosurf v1.1.1
+)
 
-require github.com/justinas/nosurf v1.1.1 // indirect
+require github.com/alexedwards/scs/v2 v2.5.1
