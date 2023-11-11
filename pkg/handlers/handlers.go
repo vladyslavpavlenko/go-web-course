1a511b4651fb059874be04ac60ff853b1ba6279c
Vladyslav Pavlenko <vladyslavpavlen@gmail.com>
2023-11-11 17:57:25 +0200

    Created a handler that returns JSON

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -1,10 +1,12 @@
 package handlers
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/vladyslavpavlenko/go-web-course/pkg/config"
 	"github.com/vladyslavpavlenko/go-web-course/pkg/models"
 	"github.com/vladyslavpavlenko/go-web-course/pkg/render"
+	"log"
 	"net/http"
 )
 
@@ -66,6 +68,27 @@ func (m *Repository) PostAvailability(w http.ResponseWriter, r *http.Request) {
 	start := r.Form.Get("start")
 	end := r.Form.Get("end")
 	w.Write([]byte(fmt.Sprintf("Start date is %s and end date is %s", start, end)))
+}
+
+type jsonResponse struct {
+	OK      bool   `json:"ok"`
+	Message string `json:"message"`
+}
+
+func (m *Repository) AvailabilityJSON(w http.ResponseWriter, r *http.Request) {
+	resp := jsonResponse{
+		OK:      true,
+		Message: "Available!",
+	}
+
+	out, err := json.MarshalIndent(resp, "", "    ")
+	if err != nil {
+		log.Println(err)
+	}
+
+	log.Println(string(out))
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(out)
 }
 
 func (m *Repository) Contact(w http.ResponseWriter, r *http.Request) {
