df8f76c38102e445383e2a35f4c32e6e6b942271
Vladyslav Pavlenko <vladyslavpavlen@gmail.com>
2024-01-08 01:30:14 +0200

    added user migration

---
+++ migrations/20240107222629_add_user_to_user_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO "public"."users"("first_name","last_name","email","password","access_level","created_at","updated_at")
+VALUES
+    (E'Vladyslav',E'Pavlenko',E'admin@admin.com',E'$2a$12$8063e1SyqHoBOw/WqSyAv.9swgQkq1Ngqjho1iSJUdNyS9BlTIAIW',3,E'2024-01-01 00:00:00',E'2024-01-01 00:00:00');
